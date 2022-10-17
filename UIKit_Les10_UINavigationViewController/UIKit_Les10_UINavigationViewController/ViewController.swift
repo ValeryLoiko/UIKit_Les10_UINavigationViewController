d1d3ab9a8e57cb908346fc28f557f1850c0405be
Walery Łojko <>
2022-10-17 11:20:56 +0200

    First commit

---
+++ UIKit_Les10_UINavigationViewController/UIKit_Les10_UINavigationViewController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit_Les10_UINavigationViewController
+//
+//  Created by Walery Łojko on 17/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
